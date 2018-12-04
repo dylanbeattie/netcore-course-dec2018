877af7a6176d9218052ddcb5c0309ddbc078bd61
Dylan Beattie <dylan@dylanbeattie.net>
2018-12-04 14:43:04 +0000

    Bitmask Pizza example and Enumerations

---
+++ day1/BitMaskPizza/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BitMaskPizza
+{
+    [Flags]
+    enum PizzaToppings {
+        Plain = 0,
+        Cheese = 1,
+        Tomato = 2,
+        Olives =4,
+        Bacon = 8,
+        Anchovies = 16,
+        Egg = 32,
+        Pepperoni = 64,
+        Mushroom = 128
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var meat = PizzaToppings.Bacon | PizzaToppings.Pepperoni;
+            var salty = meat | PizzaToppings.Anchovies;
+
+            for(var i = 0; i < 256; i++) {
+                var pizza = (PizzaToppings)i;
+                var isSalty = (pizza & salty) > 0;
+                if (isSalty) continue;
+                Console.WriteLine($"Number {i} Special! ({(PizzaToppings)i})");
+            }
+            // var Hernando = (PizzaToppings) (
+            //     PizzaToppings.Cheese
+            //     |
+            //     PizzaToppings.Tomato
+            //     |
+            //     PizzaToppings.Olives            
+            // );
+
+            // var Mark = (PizzaToppings) 
+            //     PizzaToppings.Olives | PizzaToppings.Capers | PizzaToppings.Anchovies
+            // ;
+            // Console.WriteLine((int)Hernando);
+            // Console.WriteLine((int)Mark);
+
+
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ day1/Enumerations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Enumerations
+{
+    enum DayOfTheWeek {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday    
+    }
+    
+    class Program
+    {
+
+        static void Main(string[] args) {
+            DayOfTheWeek today = (DayOfTheWeek)5;
+            Console.WriteLine(today.GetType());
+            switch(today) {
+                case DayOfTheWeek.Monday: 
+                case DayOfTheWeek.Tuesday: 
+                case DayOfTheWeek.Wednesday: 
+                case DayOfTheWeek.Thursday: 
+                case DayOfTheWeek.Friday: 
+                    Console.WriteLine("It's a normal weekday");
+                    break;
+                case DayOfTheWeek.Saturday: 
+                case DayOfTheWeek.Sunday: 
+                    Console.WriteLine("YAY!WEEKEND!");
+                    break;
+            }
+        }
+    }
+}
