69b2158a80815da644dfac76029c52aabc66af86
Dylan Beattie <dylan@dylanbeattie.net>
2018-12-04 15:17:55 +0000

    Animal example

---
+++ day1/ObjectExamples1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ObjectExamples1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lionel = new Animal();
+            Console.WriteLine(lionel.Name);
+            var spider = new Animal("Cuddles");
+            Console.WriteLine(spider.Name);
+            Console.WriteLine(lionel.WeightInKg);
+            Console.WriteLine(lionel.NumberOfLegs);
+        }
+    }
+    [Flags]
+    public enum Diet {
+        Carnivore = 1 ,
+        Herbivore = 2,
+        Furniture = 4
+    }
+
+    public class Animal {
+
+        public Animal() : this("Fluffy") { 
+
+        }
+        public Animal(string name) {
+            this.Name = name;
+        }
+        private Diet diet;
+
+        public Diet Diet {
+            get; set;
+        }
+
+        public string Name {get; set; }
+        public decimal WeightInKg {  get; set;  }
+        public bool HasATail {get;set;}
+        public bool IsItCute {get;set;}
+        public int NumberOfLegs {get;set;}
+    }
+}
