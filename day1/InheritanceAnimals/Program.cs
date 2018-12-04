0c4ab466e755d72f74891205f3f349dc0a0d1e9c
Dylan Beattie <dylan@dylanbeattie.net>
2018-12-04 15:36:48 +0000

    Added inheritance example with animals and walkies

---
+++ day1/InheritanceAnimals/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace InheritanceAnimals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cat = new Cat();
+            Console.WriteLine("Walking the cat...");
+            cat.Walkies();
+
+            var dog = new Dog();
+            Console.WriteLine("Walking the dog...");
+            dog.Walkies();
+
+            var worm = new Worm();
+            Console.WriteLine("Walking the worm...");
+            worm.Walkies();
+
+        }
+    }
+
+    public abstract class Animal {
+        public virtual string Speak() {
+            return "<random animal noise>";
+        }
+        public void Walkies() {
+            if (NumberOfLegs == 0) {
+                Console.WriteLine("Can't walk! No legs!");
+            }else {
+                for(var i = 0; i < this.NumberOfLegs; i++) {
+                    Console.WriteLine("Clomp");
+                }
+            }
+        }
+
+        protected abstract int NumberOfLegs { get; }
+
+    }
+
+    public abstract class Quadraped : Animal {
+        protected override int NumberOfLegs { get { return 4; } }
+    }
+
+    public class Cat : Quadraped {
+        public override string Speak() {
+           return("Meow!");
+        }
+    }
+
+    public class Dog : Quadraped {
+        public override string Speak() {
+            return("Woof!");
+        }
+    }
+    public class Millipede : Animal {
+        protected override int NumberOfLegs { get { return 100; } }
+    }
+
+    public class Worm : Animal {
+        protected override int NumberOfLegs { get { return 0; } }
+    }
+}
