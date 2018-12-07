f8606d4d03f01bec05cf467766b640e0a441fcea
Dylan Beattie <dylan@dylanbeattie.net>
2018-12-07 13:54:21 +0000

    Added localization examples

--- day4/Localization/Program.cs
+++ day4/Localization/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Globalization;
 
-namespace Localization
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Localization {
+    class Program {
+        static void Main(string[] args) {
+            var thread = System.Threading.Thread.CurrentThread;
+
+
+            foreach (var cultureName in new[] { "en-GB", "fr-FR", "de-DE", "ru-RU", "fr-CA" }) {
+                var culture = new CultureInfo(cultureName);
+                thread.CurrentCulture = culture;
+                thread.CurrentUICulture = culture;
+                ShowThings(thread);
+            }
             Console.WriteLine("Hello World!");
+            Console.ReadKey(false);
+        }
+
+        public static void ShowThings(System.Threading.Thread thread) {
+
+            string eszett = "ß";
+
+            var equalCurrently = eszett.Equals("ss", StringComparison.CurrentCulture);
+            var equalNormally = eszett.Equals("ss", StringComparison.InvariantCultureIgnoreCase);
+            var equalOrdinally = eszett.Equals("ss", StringComparison.Ordinal);
+            Console.WriteLine(equalOrdinally);
+
+            Console.WriteLine("fitness".Contains("ß", StringComparison.CurrentCultureIgnoreCase));
+            // Console.WriteLine("fitness".Contains("ß", StringComparison.InvariantCultureIgnoreCase));
+
+            Console.WriteLine(equalCurrently);
+            Console.WriteLine(equalNormally);
+
+            Console.WriteLine(thread.CurrentCulture);
+            Console.WriteLine(thread.CurrentUICulture);
+            Console.WriteLine(DateTime.Now.ToString("dddd dd MMMM yyyy"));
+            var x = 123456789;
+            var howHeavyIsDylan = 102.345M;
+            var y = 12345.67890M;
+
+            Console.WriteLine("Dylan weighs " + howHeavyIsDylan.ToString() + "kg");
+            Console.WriteLine(y);
         }
     }
 }
---
+++ day4/LocalizationUsingResources/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace LocalizationUsingResources {
+    class Program {
+        static void Main(string[] args) {
+            var cultureNames = new[] { "en-GB", "fr-FR", "es", "ru-RU" };
+            foreach (var cultureName in cultureNames) {
+                var culture = new CultureInfo(cultureName);
+
+                System.Threading.Thread.CurrentThread.CurrentCulture = culture;
+                System.Threading.Thread.CurrentThread.CurrentUICulture = culture;
+                Console.WriteLine(Messages.Program_HelloWorld);
+            }
+            Console.ReadKey(false);
+        }
+    }
+}
