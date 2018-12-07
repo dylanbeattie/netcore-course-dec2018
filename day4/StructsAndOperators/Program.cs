a7feb4c408106a518867ee90db770e5b87bc4ed1
Dylan Beattie <dylan@dylanbeattie.net>
2018-12-07 11:38:13 +0000

    Added DateTime and TimeSpan examples

---
+++ day4/StructsAndOperators/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StructsAndOperators {
+    class Program {
+        static void Main(string[] args) {
+
+            var d = DateTime.Now;
+            Console.WriteLine(d.ToString("F"));
+            Console.WriteLine(d.ToString("dddd dd MMMM yyyy HH:mm:ss"));
+            Console.ReadKey(false);
+        }
+
+        static void DateArithmeticExamples() {
+            var d = DateTime.Now;
+
+            var d1 = DateTime.Parse("1917-10-15 14:32:00");
+            var d2 = DateTime.Parse("1969-07-20 03:00:00");
+
+            var result = d2 - d1;
+            Console.WriteLine(result.Days + " days, " + result.Hours + " hours, " + result.Minutes + " minutes " + result.Seconds + " seconds");
+            Console.WriteLine(result.TotalHours);
+            Console.WriteLine(result.TotalMinutes);
+
+            Console.ReadKey(false);
+        }
+    }
+}
